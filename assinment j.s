53de9b9806343a955ebeeeb4fcbef89301ba9fe3
haridas27 <haridasglc45@gmail.com>
2023-06-16 15:06:27 +0530

    javass

---
+++ assinment j.s
@@ -0,0 +1,62 @@
+1) What is JavaScript?
+Ans : JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much ..
+
+2) What is the use of isNaN function?
+Ans : The isNaN() function determines whether a value is NaN when converted to a number. Because coercion inside the isNaN() function can be surprising, you may alternatively want to use Number.isNaN() .
+
+3) What is negative Infinity?
+Ans : Negative infinity is the opposite of (positive) infinity, or just negative numbers going on forever.
+
+4) Which company developed JavaScript?
+Ans: JavaScript was invented by Brendan Eich in 1995. It was developed for Netscape 2, and became the ECMA-262 standard in 1997. After Netscape handed JavaScript over to ECMA, the Mozilla foundation continued to develop JavaScript for the Firefox browser.
+
+5) What are undeclared and undefined variables?
+Ans: Undeclared variables are those that have not been declared or defined in the current scope, while undefined variables are those that have been declared but not given a value.
+
+6) Write the code for adding new elements dynamically?
+Ans: The createElement() method in JavaScript can be used to create new items dynamically.
+
+7) What is the difference between ViewState and SessionState?
+Ans: Session state is saved on the server, ViewState is saved in the page.
+
+8) What is === operator?
+Ans: Comparison Operator.
+
+9) How can the style/class of an element be changed?
+Ans: Another way to alter the style of an element is by changing its class attribute
+
+10) How to read and write a file using JavaScript?
+Ans: 
+file=fopen(getScriptPath(),0); The function fread() is used for reading the file content.
+str = fread(file,flength(file) ; The function fwrite() is used to write the contents to the file.
+file = fopen("c:\MyFile.txt", 3);// opens the file for writing.
+
+11) How can you convert the string of any base to an integer in JavaScript?
+Ans:  To convert a string to an integer parseInt(), Number(), and Unary operator(+) function is used in javascript
+
+12) What is the function of the delete operator?
+Ans: Using the delete operator on an object deallocates its memory. 
+
+13) What are all the types of Pop up boxes available in JavaScript?
+Ans: JavaScript has three kind of popup boxes: Alert box, Confirm box, and Prompt box.
+
+14) What is the use of Void (0)?
+Ans :  It is used to prevent any side effects caused while inserting an expression in a web page.
+
+15) How can a page be forced to load another page in JavaScript?
+Ans: we can use window. location object to force a page to load another page. We can use the location object to set the URL of a new page.
+
+16) What are the disadvantages of using innerHTML in JavaScript?
+Ans: Event handlers attached to any DOM element are preserved.
+Replacement is done everywhere.
+It is not possible to append innerHTML.
+Breaks the document.
+Used for Cross-site Scripting.
+
+17) Create password field with show hide functionalities 
+Ans :  task 
+
+18) Create basic math operation in JS
+Ans: task
+
+
